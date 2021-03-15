e3fd3ae51309c8205b795d00c708661b9e719161
lizhichong <lizhichong@bytedance.com>
2021-03-15 19:57:30 +0800

    feat: os
    Change-Id: Ic7e1dd5941d494a720d4c0102972385dc2af953e

---
+++ packages/os/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/lc-dmx/py2go/packages/os/utils"
+)
+
+func main() {
+	fmt.Println("----------os package----------")
+	// os 包
+	for idx, arg := range os.Args[1:] {
+		fmt.Println(utils.NumToStr(idx+1), "参数", arg)
+	}
+	fmt.Println()
+
+	fmt.Println("----------flag package----------")
+	// flag 包
+	var (
+		name = flag.String("name", "", "名称")
+		age = flag.Uint64("age", 0, "年龄")
+		height = flag.Uint64("height", 0, "身高")
+		weight = flag.Uint64("weight", 0, "体重")
+		married = flag.Bool("married", false, "是否已婚")
+	)
+	flag.Parse()
+	format := "%-10s:%v\n"
+	fmt.Printf(format, "name", *name)
+	fmt.Printf(format, "age", *age)
+	fmt.Printf(format, "height", *height)
+	fmt.Printf(format, "weight", *weight)
+	fmt.Printf(format, "married", *married)
+}
+
